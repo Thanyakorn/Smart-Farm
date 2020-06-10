49842b513c4e2f46ef65f53ef8f8a6178f51b7f6
thanyakorn.nak <thanyakorn.nak@mail.kmutt.ac.th>
2020-06-10 20:57:00 +0700

    01Moisture

---
+++ test/unit/Soil Moisture Sensor/Soil Moisture Sensor.ino
@@ -0,0 +1,28 @@
+const int analogInPin = A0; //ประกาศขา
+int Moisture = 0;        // ตัวแปรค่า Analog
+int outputValue = 0;        // ตัวแปรสำหรับ Map เพื่อคิด %
+
+void setup(){
+
+    Serial.begin(9600);
+
+
+}
+
+void loop(){
+
+  Moisture = analogRead(analogInPin);
+  outputValue = map(Moisture, 0, 1023, 100, 0); //คิดเป็น %
+
+
+  Serial.print("Moisture = ");
+  Serial.print(outputValue);
+  Serial.println(" %");
+
+  delay(1000);
+
+
+
+
+
+}
