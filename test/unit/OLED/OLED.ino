e68e45d9a3bfe7c1741ddd3d4d6818ab029b6222
thanyakorn.nak <thanyakorn.nak@mail.kmutt.ac.th>
2020-06-10 20:46:49 +0700

    01OLED

--- test/unit/OLED/OLED.ino
+++ test/unit/OLED/OLED.ino
@@ -15,52 +15,20 @@ INSTRALL FORM IDE LIBRARY MANAGER
 Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
 
 void setup() {
-  pinMode(2, INPUT_PULLUP);
-
+  
   oled.begin(0x3C); // Address 0x3C for 128x32
-
   oled.clearDisplay();
-
   oled.setCursor(0, 0);
-
   oled.setTextColor(SSD1306_WHITE);
-  oled.setTextSize(1);
-  oled.print("text 1");
-
-  oled.setTextSize(2);
-  oled.println("text 2");
-
-  oled.setTextSize(3);
-  oled.println("text 3");
-
   oled.setTextSize(1);
   oled.println("Hello World");
 
   oled.display();
 
-  delay(2000);
+  delay(1000);
 }
 
 void loop() {
-  oled.clearDisplay();
-  oled.setCursor(0, 0);
-
-  if (!digitalRead(2)) {
-    oled.println("HI 0,0");
-  }
-
-  oled.setCursor(64, 16);
-  oled.println("HI 64, 16");
-  oled.println(millis());
-
-  oled.drawLine(0, 0, 32, 32, SSD1306_WHITE);
-
-  oled.drawCircle(0, 0, 20, SSD1306_WHITE);
-  oled.drawRect(32, 0, 20, 20, SSD1306_WHITE);
-
-  oled.fillCircle(64, 0, 10, SSD1306_WHITE);
-  oled.fillRect(96, 0, 20, 10, SSD1306_WHITE);
-
-  oled.display();
-  delay(10);
+ 
+ 
 }
